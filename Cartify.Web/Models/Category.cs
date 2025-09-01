509860f3829fb7b74a8bcfdc40abc4513cb02fa1
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2025-09-01 12:00:48 +0300

    Added Category Model

---
+++ Cartify.Web/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Cartify.Web.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
+    }
+}
