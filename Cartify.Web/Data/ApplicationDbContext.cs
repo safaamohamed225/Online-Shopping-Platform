159dd891825ecfcbe7e994570ba7f3ea75997c75
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2025-09-01 14:58:42 +0300

    Add DbContext

---
+++ Cartify.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Cartify.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cartify.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {          
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
