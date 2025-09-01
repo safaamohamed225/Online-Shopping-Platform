242af79376486058a0c3c9ef2fce82fa4e3de0f2
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2025-09-01 12:03:38 +0300

    DataAnnotation on Category model

--- Cartify.Web/Models/Category.cs
+++ Cartify.Web/Models/Category.cs
@@ -1,8 +1,11 @@
-﻿namespace Cartify.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cartify.Web.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime CreatedTime { get; set; } = DateTime.Now;
