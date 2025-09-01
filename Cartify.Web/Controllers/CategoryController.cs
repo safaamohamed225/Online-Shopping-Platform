9a21d780ef97cf0b0c7bcd3a6e367af23ad944f0
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2025-09-01 17:14:14 +0300

    Category Crud Index

---
+++ Cartify.Web/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Cartify.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cartify.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
