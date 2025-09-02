af06a03957c7f06978e04dda4d358a6079c45a50
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2025-09-02 07:55:29 +0300

    feat: add server-side validation for input data

--- Cartify.Web/Models/Category.cs
+++ Cartify.Web/Models/Category.cs
@@ -7,6 +7,7 @@ namespace Cartify.Web.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [Length(3,150)]
         public string Description { get; set; }
         public DateTime CreatedTime { get; set; } = DateTime.Now;
     }
