88d86a832efdace1ebd74b5c0db0ec76b111da8e
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2025-09-04 15:17:20 +0300

    feat(Product): add ProductController with CRUD endpoints and integrated with repository & unit of work

---
+++ Cartify.Web/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,102 @@
+﻿using Cartify.DataAccess.Data;
+using Cartify.Entities.Models;
+using Cartify.Entities.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cartify.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var products = _unitOfWork.Product.GetAll();
+            return View(products);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Create(Product product)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(product);
+                _unitOfWork.Complete();
+                TempData["Create"] ="Data has been created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var product = _unitOfWork.Product.Get(c => c.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+               _unitOfWork.Product.Update(product);
+                _unitOfWork.Complete();
+                TempData["Update"] = "Data has been updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var product = _unitOfWork.Product.Get(c => c.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var product = _unitOfWork.Product.Get(c => c.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(product);
+            _unitOfWork.Complete();
+            TempData["Delete"] = "Data has been deleted successfully";
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var product = _unitOfWork.Product.Get(c => c.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+    }
+}
