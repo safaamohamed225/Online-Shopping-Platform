cd5c719e753cab88d119ed946609017e76335d60
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2025-09-04 14:37:52 +0300

    feat(models): add Product entity with validation and category relation
    - Created Product class with properties: Id, Name, Description, Price, Image
    - Added data annotations for validation (Required, Length)
    - Established foreign key relationship with Category (CategoryId, Category navigation property)

--- Cartify.DataAccess/Data/ApplicationDbContext.cs
+++ Cartify.DataAccess/Data/ApplicationDbContext.cs
@@ -11,5 +11,6 @@ namespace Cartify.DataAccess.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
---
+++ Cartify.Entities/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartify.Entities.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required, Length(5, 50)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
