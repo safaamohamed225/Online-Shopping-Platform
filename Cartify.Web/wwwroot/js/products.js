170f9a281cf92558a19c8c38ee834cd3ac4ca59f
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2025-09-05 11:06:43 +0300

    feat: integrate jQuery DataTable with MVC Product view

--- Cartify.Web/wwwroot/js/products.js
+++ Cartify.Web/wwwroot/js/products.js
@@ -8,13 +8,30 @@ $(document).ready(function () {
 function LoadDataTable() {
     dataTable = $('#DT_load').DataTable({
         "ajax": {
-            "url": "/Admin/Product/GetAll"
+            "url": "/Admin/Product/GetAll",
+            "type": "GET",
+            "datatype": "json"
         },
         "columns": [
-            { "data": "name", "width": "15%" },
-            { "data": "description", "width": "25%" },
-            { "data": "price", "width": "10%" },
-            { "data": "category.name", "width": "15%" }
-        ],
+            { "data": "name"},
+            { "data": "description"},
+            { "data": "price"},
+            { "data": "category.name" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                                <div class="text-center">
+                                    <a href="/Admin/Product/Edit/${data}" class="btn btn-success btn-sm text-white" style="cursor:pointer;">
+                                        <i class="fas fa-edit"></i> Edit
+                                    </a>
+                                    <a onclick="Delete('/Admin/Product/Delete/${data}')" class="btn btn-danger btn-sm text-white" style="cursor:pointer;">
+                                        <i class="fas fa-trash-alt"></i> Delete
+                                    </a>
+                                </div>
+                            `;
+                }
+            }
+        ]
     })
 }
