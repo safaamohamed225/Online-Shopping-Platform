8a64a7b224026d7a40d0ef0547b080d923b0c084
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2025-09-20 18:13:50 +0300

    feat(cart): implement shopping cart UI and display total price

---
+++ Cartify.Entities/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,15 @@
+﻿using Cartify.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartify.Entities.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public IEnumerable<ShoppingCart> CartsList { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
--- Cartify.Web/Areas/Customer/Controllers/CartController.cs
+++ Cartify.Web/Areas/Customer/Controllers/CartController.cs
@@ -1,13 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cartify.Entities.Repositories;
+using Cartify.Entities.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Cartify.Web.Areas.Customer.Controllers
 {
     [Area("Customer")]
+    [Authorize]
     public class CartController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM CartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
-            return View();
+            var claimsIdentity = (ClaimsIdentity)User.Identity!;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            CartVM = new ShoppingCartVM()
+            {
+                CartsList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value, includes:"Product")
+            };
+            foreach(var cart in CartVM.CartsList)
+            {
+                CartVM.CartTotal += (cart.Product.Price * cart.Count);
+            }
+            return View(CartVM);
         }
     }
 }
