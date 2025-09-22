a43f938c5ea25f59a506f6bce685b63db57cd645
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2025-09-22 09:58:26 +0300

    feat: add OrderHeader entity

---
+++ Cartify.Entities/Models/OrderHeader.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartify.Entities.Models
+{
+    public class OrderHeader
+    {
+        public int Id { get; set; }
+        public string ApplicationUserId { get; set; }
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public DateTime OrderDate { get; set; }
+        public DateTime ShippingDate { get; set; }
+        public decimal OrderTotal { get; set; }
+        public string? OrderStatus { get; set; }
+        public string? PaymentStatus { get; set; }
+        public string? TrackingNumber { get; set; }
+        public string? Carrier { get; set; }
+        public string? SessionId { get; set; }
+        public string? PaymentIntentId { get; set; }
+        public DateTime PaymentDate { get; set; }
+
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+
+    }
+}
