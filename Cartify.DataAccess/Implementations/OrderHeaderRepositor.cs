ded9d879a2aa8720063843f4ec86fce5d52bf076
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2025-09-22 18:17:18 +0300

    feat: add IOrderHeaderRepository and implementation

---
+++ Cartify.DataAccess/Implementations/OrderHeaderRepositor.cs
@@ -0,0 +1,36 @@
+﻿using Cartify.DataAccess.Data;
+using Cartify.Entities.Models;
+using Cartify.Entities.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartify.DataAccess.Implementations
+{
+    public class OrderHeaderRepositor : GenericRepository<OrderHeader>, IOrderHeaderRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public OrderHeaderRepositor(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(OrderHeader obj)
+        {
+            _db.OrderHeaders.Update(obj);
+        }
+        public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+        {
+            var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+            if (orderFromDb != null)
+            {
+                orderFromDb.OrderStatus = orderStatus;
+                if (paymentStatus != null)
+                {
+                    orderFromDb.PaymentStatus = paymentStatus;
+                }
+            }
+        }
+    }
+}
---
+++ Cartify.Entities/Repositories/IOrderHeaderRepository.cs
@@ -0,0 +1,15 @@
+﻿using Cartify.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartify.Entities.Repositories
+{
+    public interface IOrderHeaderRepository : IGenericRepository<OrderHeader>
+    {
+        void Update(OrderHeader obj);
+        void UpdateStatus(int id, string orderStatus, string? paymentStatus = null);
+    }
+}
