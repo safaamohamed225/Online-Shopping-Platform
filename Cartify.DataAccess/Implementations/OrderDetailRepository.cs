783532ad96ae7d8b9e0aa37e15b45acfbdad2b5e
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2025-09-22 18:23:25 +0300

    feat: add IOrderDetailRepository and implementation

---
+++ Cartify.DataAccess/Implementations/OrderDetailRepository.cs
@@ -0,0 +1,24 @@
+﻿using Cartify.DataAccess.Data;
+using Cartify.Entities.Models;
+using Cartify.Entities.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartify.DataAccess.Implementations
+{
+    public class OrderDetailRepository : GenericRepository<OrderDetail>, IOrderDetailRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public OrderDetailRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(OrderDetail obj)
+        {
+            _db.OrderDetails.Update(obj);
+        }
+    }
+}
