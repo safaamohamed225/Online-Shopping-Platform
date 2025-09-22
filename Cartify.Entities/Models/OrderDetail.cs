3cafa962aca8b662da50324c5d6ba713e47fed24
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2025-09-22 10:06:58 +0300

    feat: add OrderDetail entity

---
+++ Cartify.Entities/Models/OrderDetail.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartify.Entities.Models
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+
+        public int OrderId { get; set; }
+
+        [ValidateNever]
+        public OrderHeader OrderHeader { get; set; }
+        public int ProductId { get; set; }
+        [ValidateNever]
+        public Product Product { get; set; }
+        public int Count { get; set; }
+        public decimal Price { get; set; }
+    }
+}
