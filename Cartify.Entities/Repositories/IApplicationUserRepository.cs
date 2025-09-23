2137a3336a87f0b1ee83c47a048cd5f403b4c89b
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2025-09-23 05:16:37 +0300

    feat(repo): add ApplicationUser repository

---
+++ Cartify.Entities/Repositories/IApplicationUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Cartify.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartify.Entities.Repositories
+{
+    public interface IApplicationUserRepository : IGenericRepository<ApplicationUser>
+    {
+    }
+}
