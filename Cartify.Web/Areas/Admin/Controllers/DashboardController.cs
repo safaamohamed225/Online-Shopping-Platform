705b45a0d71c58668cbd9e77ed2164a68a60db6d
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2025-09-27 20:40:36 +0300

    Add admin dashboard with full control over all pages

---
+++ Cartify.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using Cartify.Entities.Repositories;
+using Cartify.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cartify.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.AdminRole)]
+    public class DashboardController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public DashboardController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
